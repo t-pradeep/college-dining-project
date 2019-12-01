7ab02fbb8ca1aafd9d033e826ec3e2fd5c156abe
Raghavendra <raghavendarm10@gmail.com>
2019-11-30 21:17:45 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import ReactNotification from 'react-notifications-component';
 import Home from './components/Home';
 import Login from './components/Login';
+import Equipment from './components/Equipment';
+import Foodlist from './components/Foodlist';
+import Schedule from './components/Schedule';
+import Request_for_sub from './components/Request_for_sub';
+import Reschedule from './components/Reschedule';
+import Requested_order from './components/Requested_order';
 import 'react-notifications-component/dist/theme.css';
 
 
@@ -32,6 +38,12 @@ function App() {
     <div>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
+      <Route path="/equipment" exact component={Equipment}/>
+      <Route path="/foodList" exact component={Foodlist}/>
+      <Route path="/schedule" exact component={Schedule}/>
+      <Route path="/request_for_sub" exact component={Request_for_sub}/>
+      <Route path="/reschedule" exact component={Reschedule}/>
+      <Route path="/requested_order" exact component={Requested_order}/>
       </div>
     </Router>
   );
